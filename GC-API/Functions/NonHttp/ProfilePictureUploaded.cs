b9ffc1eb9983422edbf5f892a3fc60d8c5c90114
steamwings <steamwings@outlook.com>
2020-10-28 23:42:56 -0400

    Actually add ProfilePictureUploaded and tests for real this time

---
+++ GC-API/Functions/NonHttp/ProfilePictureUploaded.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using Functions.Helpers;
+using Microsoft.Azure.Storage.Blob;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+namespace Functions.NonHttp
+{
+    public static class ProfilePictureUploaded
+    {
+        /// <summary>
+        /// Respond to an uploaded profile picture by saving in standard format and sizes or deleting it
+        /// </summary>
+        /// <param name="profilePic"></param>
+        /// <param name="name"></param>
+        /// <param name="log"></param>
+        /// <remarks>
+        /// TODO: Immediately revoke SAS Url used (probably requires Active Directory)
+        /// </remarks>
+        [FunctionName(nameof(ProfilePictureUploaded))]
+        public static void Run([BlobTrigger("profile-pics-uploads/{name}", Connection = "SharedUserStorage")] CloudBlockBlob profilePic,
+            [Blob("profile-pics/{name}-lg", FileAccess.Write, Connection = "SharedUserStorage")] Stream large,
+            [Blob("profile-pics/{name}-md", FileAccess.Write, Connection = "SharedUserStorage")] Stream medium,
+            [Blob("profile-pics/{name}-sm", FileAccess.Write, Connection = "SharedUserStorage")] Stream small,
+            string name, ILogger log)
+        {
+            log.LogInformation($"${nameof(ProfilePictureUploaded)} processing blob (Name:{name} \n Size: {profilePic.Properties.Length} Bytes)");
+
+            if (!Guid.TryParse(name, out _))
+            {
+                log.LogWarning("{0}: Filename '{1}' is not a guid.", nameof(ProfilePictureUploaded), name);
+                goto end;
+            }
+
+            var encoder = new PngEncoder(); // Set this to whatever we want the default format to be
+            int widthLarge = Config.Get(ConfigKeys.ProfileWidthLarge, 800);
+            int widthMedium = Config.Get(ConfigKeys.ProfileWidthMedium, 300);
+            int widthSmall = Config.Get(ConfigKeys.ProfileWidthSmall, 100);
+
+            using (var blobStream = profilePic.OpenRead())
+            {
+                try
+                {
+                    using var input = Image.Load<Rgba32>(blobStream, out IImageFormat format);
+                    log.LogTrace($"${nameof(ProfilePictureUploaded)} processing blob (Name:{name}, Format:{format.Name}");
+
+                    var sideLength = Math.Min(input.Width, input.Height);
+                    var centerCrop = new Rectangle(
+                        input.Width > input.Height ? (input.Width - sideLength) / 2 : 0,
+                        input.Height > input.Width ? (input.Height - sideLength) / 2 : 0,
+                        sideLength, sideLength);
+                    input.Mutate(x => CropExtensions.Crop(x, centerCrop));
+
+                    input.Mutate(x => x.Resize(widthLarge, widthLarge));
+                    input.Save(large, encoder);
+                    input.Mutate(x => x.Resize(widthMedium, widthMedium));
+                    input.Save(medium, encoder);
+                    input.Mutate(x => x.Resize(widthSmall, widthSmall));
+                    input.Save(small, encoder);
+                }
+                catch (Exception e) when (e is UnknownImageFormatException || e is InvalidImageContentException)
+                {
+                    // TODO Notify user the upload was invalid
+                    log.LogWarning(e, $"{nameof(ProfilePictureUploaded)} read invalid profile picture '{name}'. The image will be deleted.");
+                }
+            }
+
+        // Always delete once we're done
+        end:
+            profilePic.Delete();
+        }
+    }
+}
---
+++ GC-API/FunctionsTests/ProfilePictureUploadedTests.cs
@@ -0,0 +1,148 @@
+﻿using Functions.Helpers;
+using Functions.NonHttp;
+using FunctionsTests.Extensions;
+using FunctionsTests.Helpers;
+using Microsoft.Azure.Storage.Blob;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.PixelFormats;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FunctionsTests
+{
+    [TestClass]
+    public class ProfilePictureUploadedTests
+    {
+        private const string SampleGuid = "0f316fb1-db52-4790-b311-fe348a7a0cbe";
+        private const string SamplePicPath = "samplePic";
+        private static CloudBlobContainer PicsUploadsContainer;
+
+        [ClassInitialize]
+        public static async Task ClassInitialize(TestContext context)
+        {
+            PicsUploadsContainer = TestHelper.CreateStorageContainer(context, StorageContainer.ProfilePicsUploads);
+
+            // Download sample pic
+            using var http = new HttpClient();
+            using var downloadStream = await http.GetStreamAsync("https://via.placeholder.com/600");
+            using var fileStream = File.OpenWrite(SamplePicPath);
+            downloadStream.CopyTo(fileStream);
+        }
+
+        [TestMethod]
+        public void ScalesSamplePic()
+        {
+            var blob = PicsUploadsContainer.GetBlockBlobReference(SampleGuid);
+            var bytes = File.ReadAllBytes(SamplePicPath);
+            blob.UploadFromByteArray(bytes, 0, bytes.Length);
+            using var largeStream = new MemoryStream();
+            using var mediumStream = new MemoryStream();
+            using var smallStream = new MemoryStream();
+
+            ProfilePictureUploaded.Run(blob, largeStream, mediumStream, smallStream, SampleGuid, NullLogger.Instance);
+
+            AssertSquarePng(largeStream, ConfigKeys.ProfileWidthLarge);
+            AssertSquarePng(mediumStream, ConfigKeys.ProfileWidthMedium);
+            AssertSquarePng(smallStream, ConfigKeys.ProfileWidthSmall);
+            Assert.IsFalse(blob.Exists());
+        }
+
+        /// <summary>
+        /// Integration test to verify that scaled images are created 
+        /// and appropriately added when a profile picture is uploaded
+        /// </summary>
+        [DataRow("https://via.placeholder.com/600x50.png")]
+        [DataRow("https://via.placeholder.com/600.gif")]
+        [DataRow("https://via.placeholder.com/1000.jpg")]
+        [DataTestMethod]
+        public async Task CreatesScaledImages(string samplePicUrl)
+        {
+            var blob = PicsUploadsContainer.GetBlockBlobReference(SampleGuid);
+
+            using var http = new HttpClient();
+            using var downloadStream = await http.GetStreamAsync(samplePicUrl);
+            Assert.IsFalse(downloadStream.ToString().Contains("too big", System.StringComparison.OrdinalIgnoreCase));
+            await blob.UploadFromStreamAsync(downloadStream);
+            Assert.IsTrue(blob.Exists());
+            using var largeStream = new MemoryStream();
+            using var mediumStream = new MemoryStream();
+            using var smallStream = new MemoryStream();
+
+            ProfilePictureUploaded.Run(blob, largeStream, mediumStream, smallStream, SampleGuid, NullLogger.Instance);
+
+            AssertSquarePng(largeStream, ConfigKeys.ProfileWidthLarge);
+            AssertSquarePng(mediumStream, ConfigKeys.ProfileWidthMedium);
+            AssertSquarePng(smallStream, ConfigKeys.ProfileWidthSmall);
+            Assert.IsFalse(blob.Exists());
+        }
+
+        [TestMethod]
+        public void IgnoresInvalidFilename()
+        {
+            var badId = "10f316fb";
+            var blob = PicsUploadsContainer.GetBlockBlobReference(badId);
+            var bytes = File.ReadAllBytes(SamplePicPath);
+            blob.UploadFromByteArray(bytes, 0, bytes.Length);
+            Assert.IsTrue(blob.Exists());
+            using var largeStream = new MemoryStream();
+            using var mediumStream = new MemoryStream();
+            using var smallStream = new MemoryStream();
+
+            ProfilePictureUploaded.Run(blob, largeStream, mediumStream, smallStream, badId, NullLogger.Instance);
+
+            Assert.That.StreamNotWritten(smallStream, mediumStream, largeStream);
+            Assert.IsFalse(blob.Exists());
+        }
+
+        [TestMethod]
+        public void DeletesTextFile()
+        {
+            var blob = PicsUploadsContainer.GetBlockBlobReference(SampleGuid);
+            blob.UploadText("Sample text");
+            using var largeStream = new MemoryStream();
+            using var mediumStream = new MemoryStream();
+            using var smallStream = new MemoryStream();
+
+            ProfilePictureUploaded.Run(blob, largeStream, mediumStream, smallStream, SampleGuid, NullLogger.Instance);
+
+            Assert.That.StreamNotWritten(smallStream, mediumStream, largeStream);
+            Assert.IsFalse(blob.Exists());
+        }
+
+        [TestMethod]
+        public void DeletesInvalidPicture()
+        {
+            var blob = PicsUploadsContainer.GetBlockBlobReference(SampleGuid);
+            var bytes = File.ReadAllBytes(SamplePicPath);
+            bytes[1] = bytes[3] = 0xff; // Corrupt a little data
+            blob.UploadFromByteArray(bytes, 0, bytes.Length);
+            using var largeStream = new MemoryStream();
+            using var mediumStream = new MemoryStream();
+            using var smallStream = new MemoryStream();
+
+            ProfilePictureUploaded.Run(blob, largeStream, mediumStream, smallStream, SampleGuid, NullLogger.Instance);
+
+            Assert.That.StreamNotWritten(smallStream, mediumStream, largeStream);
+            Assert.IsFalse(blob.Exists());
+        }
+
+        private void AssertSquarePng(Stream stream, ConfigKeys width)
+        {
+            Assert.IsTrue(0 < stream.Position);
+            stream.Position = 0;
+            AssertIsSquare(Image.Load<Rgba32>(stream, out var format).Bounds(), Config.GetInt(width));
+            Assert.IsInstanceOfType(format, typeof(PngFormat));
+        }
+
+        private void AssertIsSquare(Rectangle rectangle, double sideLength = 0)
+        {
+            if (rectangle.Height != rectangle.Width || (sideLength != 0 && rectangle.Width != sideLength))
+                throw new AssertFailedException($"Rectangle had (h,w)=({rectangle.Height},{rectangle.Width}) but expected square"
+                    + (sideLength != 0 ? "of size {sideLength}." : "."));
+        }
+    }
+}
